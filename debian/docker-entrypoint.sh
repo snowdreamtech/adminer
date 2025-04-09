99a667cec900c950905095a29bbec1c64745e97f
snowdream <sn0wdr1am@qq.com>
2025-04-09 18:42:11 +0800

    feat: update Adminer plugins paths and add new plugins in Dockerfiles

--- debian/docker-entrypoint.sh
+++ debian/docker-entrypoint.sh
@@ -9,17 +9,9 @@ if [ -z "${ADMINER_SQLITE_PASSWORD}" ]; then
     }
 fi
 
-# index.php
-cat >"${ADMINER_PATH}"/index.php <<EOF
-<?php
-function adminer_object() {
-    // required to run any plugin
-    include_once "./plugins/plugin.php";
-
-    // plugins autoloader
-    foreach (glob("plugins/*.php") as \$filename) {
-        include_once "./\$filename";
-    }
+# adminer-plugins.php
+cat >"${ADMINER_PATH}"/adminer-plugins.php <<EOF
+<?php // adminer-plugins.php
 
     // designs autoloader
     \$designs = array();
@@ -27,63 +19,28 @@ function adminer_object() {
         \$designs["\$filename/adminer.css"] = basename(\$filename);
     }
     
-    // enable extra drivers just by including them
-    include "./plugins/drivers/simpledb.php";
-    include "./plugins/drivers/firebird.php";
-    include "./plugins/drivers/clickhouse.php";
     
-    \$adminer = new AdminerPlugin([]);
-
-    \$plugins = array(
-        // specify enabled plugins here
-        // new AdminerDatabaseHide(array('information_schema' , 'mysql' , 'performance_schema' , 'sys')),    // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/database-hide.php
-        new AdminerDesigns(\$designs),       // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/designs.php
-        new AdminerDumpAlter(),       // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/dump-alter.php
-        new AdminerDumpBz2(),         // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/dump-bz2.php
-        new AdminerDumpDate(),        // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/dump-date.php
-        new AdminerDumpJson(),        // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/dump-json.php
-        new AdminerDumpXml(),         // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/dump-xml.php
-        new AdminerDumpZip(),         // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/dump-zip.php
-        // new AdminerEditCalendar(),    // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/edit-calendar.php     
-        new AdminerEditForeign(),     // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/edit-foreign.php    
-        new AdminerEditTextarea(),    // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/edit-textarea.php    
-        new AdminerEnumOption(),      // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/enum-option.php  
-        new AdminerFileUpload("data/"),//https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/file-upload.php
-        new AdminerForeignSystem(),   // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/foreign-system.php     
-        //new AdminerFrames(),        // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/frames.php    
-        new AdminerJsonColumn(),      // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/json-column.php  
-        // new AdminerLoginOtp(),        // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/login-otp.php
-        // new AdminerLoginServers(),    // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/login-servers.php     
-        // TODO: inline the result of password_hash() so that the password is not visible in source codes
-		new AdminerLoginPasswordLess(password_hash("${ADMINER_SQLITE_PASSWORD}", PASSWORD_DEFAULT)),// https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/login-password-less.php         
-        // new AdminerLoginSsl(),        // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/login-ssl.php
-        // new AdminerLoginTable(),      // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/login-table.php
-        // new AdminerMasterSlave(),     // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/master-slave.php
-        new AdminerPrettyJsonColumn(\$adminer),// https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/pretty-json-column.php
-        new AdminerSlugify(),         // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/slugify.php
-        new AdminerSqlLog(),          // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/sql-log.php
-        new AdminerTablesFilter(),    // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/tables-filter.php
-        // new AdminerTinymce(),         // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/tinymce.php
-        new AdminerTranslation(),     // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/translation.php
-        new AdminerVersionNoverify(), // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/version-noverify.php
-        // new AdminerWymeditor(),       // https://raw.githubusercontent.com/vrana/adminer/refs/tags/v${ADMINER_VERSION}/plugins/wymeditor.php
+return array(
+    // You can specify all plugins here or just the ones needing configuration.
+    # new AdminerDatabaseHide(array('information_schema' , 'mysql' , 'performance_schema' , 'sys')),
+    new AdminerDatabaseHide(array()),
+    new AdminerLoginPasswordLess(password_hash("${ADMINER_SQLITE_PASSWORD}", PASSWORD_DEFAULT)),
+    new AdminerDesigns(\$designs),
     );
+?>
+EOF
     
-    /* It is possible to combine customization and plugins:
-    class AdminerCustomization extends AdminerPlugin {
-    }
-    return new AdminerCustomization(\$plugins);
-    */
-    
-    \$adminer->plugins = \$plugins;
-
-    return \$adminer;
-}
-
+# index.php
+cat >"${ADMINER_PATH}"/index.php <<EOF
+<?php
 // include original Adminer or Adminer Editor
 include "./adminer.php";
 ?>
 EOF
+
+
+
+
 
 # start nginx
 /usr/sbin/nginx -c /etc/nginx/nginx.conf
