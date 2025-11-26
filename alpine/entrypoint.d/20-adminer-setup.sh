8a3460731d3678d94c2b343e95ebf0637a24c5d7
snowdreamtech <snowdreamtech@qq.com>
2025-11-26 21:02:06 +0800

    Refactor Adminer setup scripts: Update plugin configuration logic and improve debug output

--- alpine/entrypoint.d/20-adminer-setup.sh
+++ alpine/entrypoint.d/20-adminer-setup.sh
@@ -3,7 +3,6 @@ set -e
 
 if [ "$DEBUG" = "true" ]; then echo "→ [adminer] Setting up configuration"; fi
 
-
 # Random ADMINER_SQLITE_PASSWORD Generator
 if [ -z "${ADMINER_SQLITE_PASSWORD}" ]; then
     {
@@ -13,49 +12,102 @@ if [ -z "${ADMINER_SQLITE_PASSWORD}" ]; then
 fi
 
 # adminer-plugins.php
-cat >"${ADMINER_PATH}"/adminer-plugins.php <<EOF
-<?php // adminer-plugins.php
+if [ "$DEBUG" = "true" ]; then
+    rm -rfv "${ADMINER_PLUGINS_PATH}"/login-servers.php
 
-// designs autoloader
-\$designs = array();
-foreach (glob("designs/*", GLOB_ONLYDIR) as \$filename) {
-    \$designs["\$filename/adminer.css"] = basename(\$filename);
-}
+    cat >"${ADMINER_PATH}"/adminer-plugins.php <<EOF
+    <?php // adminer-plugins.php
+
+    // designs autoloader
+    \$designs = array();
+    foreach (glob("designs/*", GLOB_ONLYDIR) as \$filename) {
+        \$designs["\$filename/adminer.css"] = basename(\$filename);
+    }
 
 
-return array(
-    // You can specify all plugins here or just the ones needing configuration.
-    # new AdminerDatabaseHide(array('information_schema' , 'mysql' , 'performance_schema' , 'sys')),
-    new AdminerDatabaseHide(array()),
-    new AdminerLoginPasswordLess(password_hash("${ADMINER_SQLITE_PASSWORD}", PASSWORD_DEFAULT)),
-    new AdminerDesigns(\$designs),
-    new AdminerLoginServers(array(
-        'localhost' => 'localhost',
-        '127.0.0.1' => '127.0.0.1',
-        'db' => 'db',
-        'mysql' => 'mysql',
-        'mariadb' => 'mariadb',
-        'sqlite' => 'sqlite',
-        'postgres' => 'postgres',
-        'pg' => 'pg',
-        'oracle' => 'oracle',
-        'mssql' => 'mssql',
-        'clickhouse' => 'clickhouse',
-        'es' => 'es',
-        'elastic' => 'elastic',
-        'firebird' => 'firebird',
-        'mongodb' => 'mongodb',
-        'mongo' => 'mongo',
-        'simpledb' => 'simpledb',
-        'imap' => 'imap',
-    )),
-    new AdminerLoginIp(array()),
-    new AdminerLoginSsl(array()),
-    # new AdminerLoginOtp(null),
-    # new AdminerLoginTable(array()),
-    new AdminerMasterSlave(array()),
-);
-?>
+    return array(
+        // You can specify all plugins here or just the ones needing configuration.
+        # new AdminerDatabaseHide(array('information_schema' , 'mysql' , 'performance_schema' , 'sys')),
+        new AdminerDatabaseHide(array()),
+        new AdminerLoginPasswordLess(password_hash("${ADMINER_SQLITE_PASSWORD}", PASSWORD_DEFAULT)),
+        new AdminerDesigns(\$designs),
+        # new AdminerLoginServers(array(
+        #     'localhost' => 'localhost',
+        #     '127.0.0.1' => '127.0.0.1',
+        #     'db' => 'db',
+        #     'mysql' => 'mysql',
+        #     'mariadb' => 'mariadb',
+        #     'sqlite' => 'sqlite',
+        #     'postgres' => 'postgres',
+        #     'pg' => 'pg',
+        #     'oracle' => 'oracle',
+        #     'mssql' => 'mssql',
+        #     'clickhouse' => 'clickhouse',
+        #     'es' => 'es',
+        #     'elastic' => 'elastic',
+        #     'elasticsearch' => 'elasticsearch',
+        #     'firebird' => 'firebird',
+        #     'mongodb' => 'mongodb',
+        #     'mongo' => 'mongo',
+        #     'simpledb' => 'simpledb',
+        #     'imap' => 'imap',
+        # )),
+        new AdminerLoginIp(array()),
+        new AdminerLoginSsl(array()),
+        # new AdminerLoginOtp(null),
+        # new AdminerLoginTable(array()),
+        new AdminerMasterSlave(array()),
+    );
+    ?>
 EOF
+else
+    cat >"${ADMINER_PATH}"/adminer-plugins.php <<EOF
+    <?php // adminer-plugins.php
+
+    // designs autoloader
+    \$designs = array();
+    foreach (glob("designs/*", GLOB_ONLYDIR) as \$filename) {
+        \$designs["\$filename/adminer.css"] = basename(\$filename);
+    }
+
+
+    return array(
+        // You can specify all plugins here or just the ones needing configuration.
+        # new AdminerDatabaseHide(array('information_schema' , 'mysql' , 'performance_schema' , 'sys')),
+        new AdminerDatabaseHide(array()),
+        new AdminerLoginPasswordLess(password_hash("${ADMINER_SQLITE_PASSWORD}", PASSWORD_DEFAULT)),
+        new AdminerDesigns(\$designs),
+        new AdminerLoginServers(array(
+            'localhost' => 'localhost',
+            '127.0.0.1' => '127.0.0.1',
+            'db' => 'db',
+            'mysql' => 'mysql',
+            'mariadb' => 'mariadb',
+            'sqlite' => 'sqlite',
+            'postgres' => 'postgres',
+            'pg' => 'pg',
+            'oracle' => 'oracle',
+            'mssql' => 'mssql',
+            'clickhouse' => 'clickhouse',
+            'es' => 'es',
+            'elastic' => 'elastic',
+            'elasticsearch' => 'elasticsearch',
+            'firebird' => 'firebird',
+            'mongodb' => 'mongodb',
+            'mongo' => 'mongo',
+            'simpledb' => 'simpledb',
+            'imap' => 'imap',
+        )),
+        new AdminerLoginIp(array()),
+        new AdminerLoginSsl(array()),
+        # new AdminerLoginOtp(null),
+        # new AdminerLoginTable(array()),
+        new AdminerMasterSlave(array()),
+    );
+    ?>
+EOF
+fi
+
+
 
 if [ "$DEBUG" = "true" ]; then echo "→ [adminer] Done."; fi
